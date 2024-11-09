a20cf41b6021f9bb313e950c0564b55918645796
Rodrigo Barbosa <rodbarbosa.dv@gmail.com>
2024-11-09 13:27:39 -0300

    Criacao Projeto - Grpc - Remote Procedure Call

---
+++ grpc-framework/src/test/java/com/projeto_grpc_framework/grpc_framework/GrpcFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto_grpc_framework.grpc_framework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcFrameworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
